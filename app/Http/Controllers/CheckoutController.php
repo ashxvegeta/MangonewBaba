0ac893ffaecf1de19d5b77820f70d4b4a6c4d733
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-15 22:26:07 +0530

    remove product from cart if it is out of stock before going to checkout page by clicking proceed to checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\Product;
 
 class CheckoutController extends Controller
 {
@@ -12,6 +13,17 @@ class CheckoutController extends Controller
     public function Checkout()
     {
         
+        $oldcartItems = Cart::where('user_id', session('user')->id)->get();
+        //check for out of stock items
+        foreach($oldcartItems as $item) {
+           if(!Product::where('id', $item->prod_id)->where('qty', '>=', $item->prod_qty)->exists()) {
+               $removeItem = Cart::where('user_id', session('user')->id)
+                   ->where('prod_id', $item->prod_id)
+                   ->first();
+                   $removeItem->delete();
+           }
+        }
+        // get the fresh cart items after deletion the out of stock items
         $cartItems = Cart::where('user_id', session('user')->id)->get();
         if ($cartItems->isEmpty()) {
             return redirect()->route('cart.view')->with('error', 'Your cart is empty. Please add items to your cart before proceeding to checkout.');
